cfadb4202f666d57106b8f1287a1ac1abd78dc40
corix <rico.c.marcelo@gmail.com>
2012-04-26 19:31:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VehicleDetails::Application.config.secret_token = 'e3c96997ac33bbd73da9349acd198b9acf48ce0d07573074cba8b2a8993f8677751a37e830fd4fd7b277e82520a5bccf083b43cc09f35e340e716c6b0349ab99'
